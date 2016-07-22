eef1a70d1aa69edc8fb38823cec25b654e2ea747
wonyk <yingkeatwon@gmail.com>
2016-07-22 13:27:30 +0000

    I try

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app = express();
  
 //app.set('port', process.env.PORT || 8080);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
 
 app = config(app);
 
