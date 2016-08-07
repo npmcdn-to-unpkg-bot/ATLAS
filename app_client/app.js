ae02eea209eb555e3da15658c372c35c1c1cb2ef
wonyk <yingkeatwon@gmail.com>
2016-08-07 05:56:23 +0000

    Refreshed Look

--- app_client/app.js
+++ app_client/app.js
@@ -1,57 +1,23 @@
 (function(){
     
     angular
-        .module('Atlas', ['ui.router']);
+        .module('Atlas', ['ngRoute']);
         
     
-        function routing ($stateProvider, $urlRouterProvider) {
+        function routing ($routeProvider) {
     
-    $stateProvider
-        .state('index', {
-            url: '',
-            abstract: true,
-            templateUrl: 'index.html'
+    $routeProvider
+        .when('/', {
+            templateUrl: '/home/home.view.html',
+            controller: 'homeCtrl',
+            controllerAs: 'vm'
         })
-        .state('index.home', {
-            url: '/',
-            template: "/hone/home.view.html",
-            controller: "homeCtrl"
-        });
-                /*"index": {templateUrl: "/home/home.view.html"},
-                "contact": {templateUrl: '/nested/contact.view.html'},
-                "home" : {templateUrl: '/nested/home.view.html'}
-            }
-        })
-        .state('contact', {
-            url: "/contact",
-            parent: 'index',
-            templateUrl: "/nested/contact.view.html"
-        })
-        .state('home', {
-            url: "/home",
-            parent: 'index',
-            templateUrl: '/nested/home.view.html'
-        })
-        .state('notify-wrap', {
-            url: '/notify-wrap',
-            parent:'index',
-            templateUrl: '/nested/notify-wrap.view.html'
-        })
-        .state('side-left',{
-            url: '/side-left',
-            templateUrl: '/nested/side-left.html'
-        })
-        .state('side-right', {
-            url:'/side-right',
-            templateUrl: '/nested/side-right.html'
-        });*/
-        
-        $urlRouterProvider.otherwise({redirectTo: '/'});
 
+        .otherwise({redirectTo: '/'});
 }
    angular
     .module('Atlas')
-    .config(['$stateProvider', '$urlRouteProvider', routing]);
+    .config(['$routeProvider', routing]);
     
     
 })();
