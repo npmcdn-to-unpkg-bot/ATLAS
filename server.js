66a5fbf6386f3fb48321aabdf3e20226046782dd
wonyk <yingkeatwon@gmail.com>
2016-08-16 12:16:57 +0000

    Just for openshift

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express'),
 config = require('./server/configure'),
 app = express();
  
-app.set('port', process.env.PORT || 8080);
-//app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); //for openshift
-//app.set('ip', process.env.OPENSHIFT_NODEJS_IP);  //for openshift
+//app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); //for openshift
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP);  //for openshift
 
 app = config(app);
 
-//var server = app.listen(app.get('port'), app.get('ip'), function(){
-var server = app.listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), app.get('ip'), function(){
+//var server = app.listen(app.get('port'), function(){
     console.log("Server up: https://atlas-wonyk.c9users.io:"+ app.get('port'));
 });
 
